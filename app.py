f3b8b82f7834374947f8975279adab8239aef033
Konstantinos <tournas62@gmail.com>
2024-12-22 16:14:34 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+
+@st.cache_resource
+def get_model():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForSequenceClassification.from_pretrained('tournas/FineTuneBert')
+    return tokenizer, model
+
+tokenizer, model = get_model()
+
+user_input = st.text_area('Enter Text to Analyze')
+button = st.button('Analyze')
+
+d = {
+
+    1: 'Toxic',
+    0: 'Non Toxic'
+}
+
+if user_input and button:
+    st.write('Analyzing...please wait.')
+
+    # Tokenization with padding and truncation
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512, return_tensors='pt')
+
+    # Get output from the model
+    output = model(**test_sample)
+
+    # Softmax for probabilities
+    probs = torch.softmax(output.logits, dim=-1).detach().numpy()
+    # Appearing the probs
+    st.write(f'Probs: {probs[0][0]:.2f} | {probs[0][1]:.2f}')
+
+    # Prediction: Find the class with the highest probability
+    y_pred = np.argmax(probs, axis=1)
+    st.write('prediction: ',d[y_pred[0]])
